9042d618b37d07c899a66487ef91b75d90661c30
omkar <omkarbulbule7@gmail.com>
2025-06-24 18:02:33 +0530

    done the insertPosition and added the DSA sheet

--- insertPosition/practice.py
+++ insertPosition/practice.py
@@ -1,26 +1,24 @@
 #simple binary search algo practice.
 
-def BSAlgo(arr, x):
+def BSAlgo(arr, target):
     """
     1. first we devide the arr in two half (left and right)
-    2. then we pick the mid ele from that array
-    3. on besis of that element we search the target value
-    4. if the target is less then mid then we remove the first half from the array
-    5. wise versa to the right as well - left = mid - 1
-    6. and again we split that specifed arr to two sections left/right
-    7. the base case is we return the middle element as the target element by comparing!
+    2. adding left and right and round up to get the mid.
+    3. if the mid is equal to x then return the mid.
+    4. if the arr[mid] as arr element of that indices is smaller that x, then assing the mid to rigth and decreament by 1.
+    5. if the arr[mid] as arr element of that indices is greater than x, then assign the mid to left and indrease by 1
+    6. then it round of that left and right and get the mid again.
     """
-
-    left = 0
-    right = len(arr) - 1
+    left, right = 0, len(arr)-1
+    result = 0
     while (left<right):
         mid = (left+right)//2
-        if arr[mid] == x:
-            return mid
-        elif arr[mid] < x:
-            left = mid + 1
-        else:
+        if arr[mid] >= target:
+            result = mid
             right = mid - 1
+        else:
+            left = mid + 1
+        
+    return result
 
-
-print(BSAlgo([3, 2, 5, 6, 4], 3))
+print(BSAlgo([2, 4, 6, 8, 10], 3))
