09e6fe0d18279930e1d051de5c3f13418e78ec64
omkar <omkarbulbule7@gmail.com>
2026-02-12 07:18:15 +0530

    array two pointes oppsoite file changes

--- twoPointer/opposite.py
+++ twoPointer/opposite.py
@@ -67,28 +67,6 @@ print(isPali_copression("A man, a plan, a canal: Panama"))
 
 
 
-def two_sum_pra(arr: list[int], target: int) -> list[int]:
-    """
-    we solve this by two pointers patterns - Opposite.
-    it's only work effeciently if the array is sorted.
-    we can track a two pointers and take it's sum and compare with the target.
-    """
-    left: int = 0
-    right: int = len(arr)-1
-    while left <= right:
-        total: int = arr[left] + arr[right]
-        if total == target:
-            return [left, right]
-        elif target > total:
-            left += 1
-        else:
-            right -= 1
-
-    return [0, 0]
-
-
-
-
 #COntainer with most water -- we will solve this letter.
 
 def contain_most_water(arr: list[int]) -> int:
@@ -113,18 +91,12 @@ print(contain_most_water([0,2]))
 #the three sum in sorted array means -
 #any distinct elements from sorted array and it's sum shoudl be eqaul to 0.
 def three_sum(arr: list[int]) -> list[int]:
-    """
-    so in two pointer - opposite direction pattern. 
-    how should we calculate the three distinct values.
-    i think we have to add the condition here. where,
-    if the addition of first left+right is equal to 0 or gt(- or +) way then we right-=1 otherwise left+=1
-    """
-    arr.sort()
+    arr.sort() #-----------sorting help us to apply the opposite-two-pointers pattrn.
     total = 0
     res = []
     for i in range(len(arr)-2):
-        if i > 0 and arr[i] == [i-1]:
-            continue
+        if i > 0 and arr[i] == [i-1]: 
+            continue# -----------skiping the iteration if i gt 0 and 0th and 1st are equal... continue
 
         left: int = i+1
         right: int = len(arr)-2
@@ -152,31 +124,3 @@ def three_sum(arr: list[int]) -> list[int]:
     return res
 
 print(three_sum([-1, 0, 1, 2, -1, -4]))
-
-
-#valid palindrom --
-def valid_apli(s: str) -> bool:
-    """
-    we use the two pointer here - where each pointer check is it similar if yes them move inward.
-    also check is is isalnum() or not if not them move inward. 
-    """
-    left = 0
-    right = len(s)-1
-    while left < right:
-        while left < right and not s[left].isalnum():
-            left += 1
-
-        while left < right and not s[right].isalnum():
-            right -= 1
-
-        if s[left].lower() != s[right].lower():
-            return False
-            
-        left += 1
-        right -= 1
-
-
-    return True
-
-print(valid_apli("rracecar"))
-
