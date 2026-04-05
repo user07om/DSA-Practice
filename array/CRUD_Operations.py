a8b1def392eb97d3937765b910395f34e0108df6
omkar <omkarbulbule7@gmail.com>
2026-04-06 01:15:08 +0530

    feat(array): optimized zero shifting using two-pointer approach instead of brute suffering

--- array/CRUD_Operations.py
+++ array/CRUD_Operations.py
@@ -82,3 +82,86 @@ def delValue(arr, val):
     return arr
 
 print(delValue(arr, 2))
+
+
+#move all zeros to the end: time complexity is O(n) and space is None. O(1)
+arr = [0, 1, 1, 0, 2, 1] # [_, _, _, _, 0, 0] non-zero elements order does not matter.
+
+#the bruteforce approach would be -> just remove the zeros and also keep the count. 
+#add the zeros as count times in orignal array.
+
+count = arr.count(0)
+
+while 0 in arr:
+    key = 0
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            key = i
+
+    for i in range(key, len(arr)-1):
+        arr[i] = arr[i+1]
+
+    arr.pop()
+
+for _ in range(count):
+    arr.append(0)
+print(arr)
+
+
+
+# the second approach is array rotation - it's not correct answer bcause as per the problem statement
+# we have to keep the order of non-zero elements in order. and in this solution we need to sort an array. So!
+arr = [0, 1, 1, 0, 2, 1] # [_, _, _, _, 0, 0] non-zero elements order does not matter.
+
+def rotMoveZero(arr):
+    n = len(arr)
+    def reverse(arr, left, right):
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+
+    if 0 in arr:
+        arr.sort()
+        count = sum(1 for x in arr if x == 0)
+
+        
+        reverse(arr, 0, n-1)#reverse the entire array so the zeros will be pushed to the end.
+        reverse(arr, 0, n-count-1)#only reverse the non-zero elements.
+
+
+
+    return arr
+
+print(rotMoveZero(arr))
+
+
+#the only and optimal solution is fast ans slow pointer in two variations: 
+# 1. keep the track of 0's as slow poiner and replace it with non-zero element.
+# 2. - get the count of non-zero elements.
+#    - read the entire array as fast pointer and replace the current element with 0. 
+arr = [0, 1, 1, 0, 2, 1] # [_, _, _, _, 0, 0] non-zero elements order does not matter.
+
+def OptiZero(arr):
+    slow = 0
+    for fast in range(len(arr)):
+        if arr[fast] != 0:
+            nums[fast], nums[slow] = nums[slow], nums[fast]
+            #arr[slow], arr[fast] = arr[slow], arr[fast] this is the problem man!
+            slow += 1
+    return arr
+
+nums = [0, 1, 1, 0, 0, 2]
+def OptiZeroTwo(nums):
+    k = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[k] = nums[i]
+            k += 1
+
+    for i in range(k, len(nums)):
+        nums[i] = 0
+
+    return nums
+
+print(OptiZeroTwo(nums))
