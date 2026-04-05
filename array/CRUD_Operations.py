8067adf901628ec2db1b92fc6761fa01e9065111
omkar <omkarbulbule7@gmail.com>
2026-04-05 06:09:16 +0530

    feat(array): added deletion ops (first, index, value) with unnecessary manual loops

--- array/CRUD_Operations.py
+++ array/CRUD_Operations.py
@@ -2,8 +2,9 @@
 
 
 print("==========================================================================================================================================================")
-#insert elemet at verious position
+# insert elemet at verious position
 # insert at beginning
+# insert at end
 arr = [1, 2, 3, 4, 5]
 val = 9
 arr.append(0)
@@ -46,3 +47,38 @@ for i in range(len(new_arr)):
         new_arr[i] = arr[i-1]
 
 print(new_arr)
+
+
+print("==========================================================================================================================================================")
+# delete first element 
+# delete at index
+# delete by value
+
+arr = [1, 2, 3, 4, 5]
+
+def delFirst(arr):
+    for i in range(1, len(arr)):
+        arr[i-1] = arr[i]
+    arr.pop()
+    return arr
+
+def delIndex(arr, idx): #non zero based - means it's start from 0
+    for i in range(idx, len(arr)): #if wants zero based, then idx+1..n
+        arr[i-1] = arr[i]
+    arr.pop()
+    return arr
+
+def delValue(arr, val):
+    #first get the idx by val checking
+    idx = 0
+    for i in range(len(arr)):
+        if arr[i] == val:
+            idx = i
+
+    for i in range(idx+1, len(arr)):
+        arr[i-1] = arr[i]
+    arr.pop()
+
+    return arr
+
+print(delValue(arr, 2))
