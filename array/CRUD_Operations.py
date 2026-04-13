088449542806db31a2fb79780439154f3222d73e
omkar <omkarbulbule7@gmail.com>
2026-04-13 06:57:26 +0530

    array practice

--- array/CRUD_Operations.py
+++ array/CRUD_Operations.py
@@ -1,252 +1,356 @@
-print("==========================================================================================================================================================")
-# insert elemet at verious position
-# insert at beginning
-# insert at end
-arr = [1, 2, 3, 4, 5]
-val = 9
-arr.append(0)
-for i in range(len(arr)-1, 0, -1):
-    arr[i] = arr[i-1]
-arr[0] = val
 
-#insert at position: there are many variations:
-arr = [1, 2, 3, 4, 5]
-val = 9; idx = 2
-arr.append(0)
-n = len(arr)
+class InsertVal:
+    """
+        insert at position: there are many variations:
+        - insert elemet at verious position
+        - insert at beginning
+        - insert at end
+    """
+    def __init__(self, arr):
+        self.arr = arr
 
-print(arr)
-for i in range(n-1, -1, -1):
-    arr[i] = arr[i-1]
-    if i == idx:
-        arr[idx] = val
-        break
-print(arr)
+    def reverse_way(self, idx, val):
+        self.arr.append(0) #for making the space in arr
+        n = len(self.arr)
+        for i in range(n-1, idx, -1):
+            self.arr[i] = self.arr[i-1]
 
-arr = [1, 2, 3, 4, 5]
-arr.append(0)
-temp = val
-for i in range(idx, len(arr)):
-    curr_val = arr[i]
-    arr[i] = temp
-    temp = curr_val
+        self.arr[idx] = val
 
-print(arr)
+        return self.arr
 
-arr = [1, 2, 3, 4, 5]
-new_arr = [0]*(len(arr)+1)
-for i in range(len(new_arr)):
-    if i < idx:
-        new_arr[i] = arr[i]
-    elif i == idx:
-        new_arr[i] = val
-    else:
-        new_arr[i] = arr[i-1]
+    def variable_way(self, idx, val):
+        self.arr.append(0) 
+        n = len(self.arr)
+        temp = val
+        for i in range(idx, n):
+            curr_ele = self.arr[i]
+            self.arr[i] = temp
+            temp = curr_ele
 
-print(new_arr)
+        return self.arr
 
+    def extra_space_way(self, idx, val):
+        n = len(self.arr)
+        temp = [0]*(n+1)
+        for i in range(len(temp)):
+            if i < idx:
+                temp[i] = self.arr[i]
+            elif i == idx:
+                temp[i] = val
+            else:
+                temp[i] = self.arr[i-1]
 
-print("==========================================================================================================================================================")
-# delete first element 
-# delete at index
-# delete by value
+        return temp
 
-arr = [1, 2, 3, 4, 5]
-
-def delFirst(arr):
-    for i in range(1, len(arr)):
-        arr[i-1] = arr[i]
-    arr.pop()
-    return arr
-
-def delIndex(arr, idx): #non zero based - means it's start from 0
-    for i in range(idx, len(arr)): #if wants zero based, then idx+1..n
-        arr[i-1] = arr[i]
-    arr.pop()
-    return arr
-
-arr = [1, 2, 3, 4, 5]; val = 2
-def delValue(arr, val):
-    #first get the idx by val checking
-    idx = 0
-    for i in range(len(arr)):
-        if arr[i] == val:
-            idx = i
-
-    for i in range(idx, len(arr)-1):
-        arr[i] = arr[i+1]
-    arr.pop()
-
-    return arr
-
-print(delValue([1, 2, 3, 4, 5], 2), "yoo--------------------")
+InsertPos = InsertVal([1, 2, 3, 4, 5])
+#opr_one = InsertPos.reverse_way(2, 9)
+#opr_two = InsertPos.variable_way(2, 9)
+opr_three = InsertPos.extra_space_way(2, 9)
+print(opr_three, " operations----------------------------------------------> ")
 
 
-#move all zeros to the end: time complexity is O(n) and space is None. O(1)
-arr = [0, 1, 1, 0, 2, 1] # [_, _, _, _, 0, 0] non-zero elements order does not matter.
 
-#the bruteforce approach would be -> just remove the zeros and also keep the count. 
-#add the zeros as count times in orignal array.
+class DeleteVal:
+    """
+        - delete first element 
+        - delete at index
+        - delete by value
+    """
 
-count = arr.count(0)
+    def __init__(self, arr):
+        self.arr = arr
 
-while 0 in arr:
-    key = 0
-    for i in range(len(arr)):
-        if arr[i] == 0:
-            key = i
+    def del_first(self):
+        n = len(self.arr)
+        for i in range(1, n):
+            self.arr[i-1] = self.arr[i]
 
-    for i in range(key, len(arr)-1):
-        arr[i] = arr[i+1]
-
-    arr.pop()
-
-for _ in range(count):
-    arr.append(0)
-
-print([0, 1, 2, 0], "does it make sense, this won't work.")
-
-# the second approach is array rotation - it's not correct answer bcause as per the problem statement
-# we have to keep the order of non-zero elements in order. and in this solution we need to sort an array. So!
-arr = [0, 1, 1, 0, 2, 1] # [_, _, _, _, 0, 0] non-zero elements order does not matter.
-
-def rotMoveZero(arr):
-    n = len(arr)
-    def reverse(arr, left, right):
-        while left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
-
-    if 0 in arr:
-        arr.sort()
-        count = sum(1 for x in arr if x == 0)
-
+        self.arr.pop()
         
-        reverse(arr, 0, n-1)#reverse the entire array so the zeros will be pushed to the end.
-        reverse(arr, 0, n-count-1)#only reverse the non-zero elements.
+        return self.arr
+
+    def del_at_idx(self, idx):
+        n = len(self.arr)
+        #for i in range(idx, n-1):
+            #self.arr[i] = self.arr[i+1]
+
+        for i in range(idx+1, n):
+            self.arr[i-1] = self.arr[i]
+        self.arr.pop()
+
+        return self.arr
+
+    def del_val(self, val):
+        n = len(self.arr)
+        key = 0
+        for i in range(n):
+            if self.arr[i] == val:
+                key = i
+
+        for i in range(key, n-1):
+            self.arr[i] = self.arr[i+1]
+
+        self.arr.pop()
+
+        return self.arr
+
+
+delVal = DeleteVal([1,2,3,4,5])
+#first = delVal.del_first()
+#second = delVal.del_at_idx(2)
+third = delVal.del_val(4)
+print(third, " -----------------------------------> omkar here i'm")
 
 
 
-    return arr
-
-print(rotMoveZero(arr))
 
 
-#the only and optimal solution is fast ans slow pointer in two variations: 
-# 1. keep the track of 0's as slow poiner and replace it with non-zero element.
-# 2. - get the count of non-zero elements.
-#    - read the entire array as fast pointer and replace the current element with 0. 
-arr = [0, 1, 1, 0, 2, 1] # [_, _, _, _, 0, 0] non-zero elements order does not matter.
+class PartitionProblems:
+    """in the partition problem the zero pattern is also there but it's okay! we're not adding in this class"""
+    def __init__(self, arr):
+        self.arr = arr
+        self.n = len(self.arr)
 
-def OptiZero(arr):
-    slow = 0
-    for fast in range(len(arr)):
-        if arr[fast] != 0:
-            arr[fast], arr[slow] = arr[slow], arr[fast]
-            #arr[slow], arr[fast] = arr[slow], arr[fast] this is the problem man!
-            slow += 1
-    return arr
-print(OptiZero(arr), "Fuck it")
+    def brute_force(self): #this is not working!
+        n = len(self.arr)
+        cnt = 0
+        for i in range(n):
+            if self.arr[i] == 0:
+                cnt += 1
 
-nums = [0, 1, 1, 0, 0, 2]
-def OptiZeroTwo(nums):
-    k = 0
-    for i in range(len(nums)):
-        if nums[i] != 0:
-            nums[k] = nums[i]
-            k += 1
-
-    for i in range(k, len(nums)):
-        nums[i] = 0
-
-    return nums
-
-print(OptiZeroTwo(nums))
-
-arr = [1,2,3,2,4,2]; k = 2
-
-def remOccur(arr, k):
-    n = len(arr)
-    s = 0
-    for i in range(n-1):
-        if arr[i] != k:
-            arr[s] = arr[i]
-            s += 1
-
-    return arr[:s]
-
-print(remOccur(arr, k), "yoo------------------remoccr--")
+                for j in range(n-1, n-i-1, -1):
+                    self.arr[j] = self.arr[j-1]
 
 
-arr = [1,1,2,2,3,3]
-def remDubli(arr):
-    n = len(arr)
-    u = 0
-    for r in range(n-1):
-        if arr[r] != arr[u]:
-            u += 1
-            arr[u] = arr[r]
+        for _ in range(cnt):
+            self.arr.pop()
 
-    for i in range(u+1):
-        arr.pop()
+        for _ in range(cnt):
+            self.arr.append(0)
 
-    return arr
+        return self.arr
 
-print(remDubli(arr), "yoo------------------remdubli--")
+    def rotate_zero(self):
+        self.arr.sort()
+        n = len(self.arr)
+        zeros = self.arr.count(0)
 
-arr = [1,-2,3,-4,5]
-def isSap(arr):
-    count = 0
-    for i in range(len(arr)-1):
-        if arr[i] >= 0 and arr[i+1] < 0:
-            count += 1
-    if count > 1: 
-        return False
-    return True
+        def rev_arr(arr, left, right):
+            while left < right:
+                arr[left], arr[right] = arr[right], arr[left]
+                left += 1
+                right -= 1
 
-def mvNgLeft(arr):
-    n = len(arr)
-    for i in range(len(arr)-1):
-        if arr[i] < 0:
-            arr[i], arr[i+1] = arr[i+1], arr[i]
+            return arr
+
+        #rev_arr(self.arr, 0, zeros)
+        rev_arr(self.arr, zeros, n-1)
+        rev_arr(self.arr, 0, n-1)
+
+        return self.arr
+
+    def optimal_one(self):
+        n = len(self.arr)
+        slow = 0
+        for fast in range(n):
+            if self.arr[fast] != 0:
+                self.arr[fast], self.arr[slow] = self.arr[slow], self.arr[fast]
+                slow += 1
+
+        return self.arr
+
+    def optimal_two(self):
+        n = len(self.arr)
+        slow = 0
+        for fast in range(n):
+            if self.arr[fast] != 0:
+                self.arr[slow] = self.arr[fast]
+                slow += 1
+
+        for i in range(slow, n):
+            self.arr[i] = 0
+            
+        return self.arr
+
+    def rem_element(self, k): #same fast and slow pointer approach! if it's not equal to k then swap with fast and slow+=1.
+        s = 0
+        for i in range(self.n):
+            if self.arr[i] != k:
+                self.arr[s] = self.arr[i]
+                s += 1
+
+        return self.arr[:s]
+            
+    def rem_dublicates(self): #using fast and slow pointer, the past ele at slow pointer are correct and other needs correction.
+        s = 0
+        for i in range(self.n):
+            if self.arr[i] != self.arr[s]:
+                s += 1
+                self.arr[s] = self.arr[i]
+
+        return self.arr[:s+1]
+
+    #bruteforce approach to move negetive to left.
+    def negative_partition(self): #it has some issues, cannot solve this right now?
+        s = 0
+        for i in range(self.n):
+            if self.arr[i] < 0:
+                self.arr[s] = self.arr[i]
+                s += 1
+
+        if not self.isSap():
+            self.negative_partition()
+
+        return self.arr
+
+    def isSap(self):
+        count = 0
+        for i in range(self.n-1):
+            if self.arr[i] >= 0 and self.arr[i+1] < 0:
+                count += 1
+
+        print(count)
+        return count == 1
+
+    def mvNeg_slow_fast(self): #read the array using fast and do the modification using slow pointer.
+        s = 0
+        for i in range(len(self.arr)):
+            if self.arr[i] >= 0:
+                self.arr[i], self.arr[s] = self.arr[s], self.arr[i]
+                s += 1
+
+        return self.arr
+
+    def mvNeg_opposite_pointer(self): #we change the l or r pointer by chekcing it's negative or not and then we swap it.
+        l = 0
+        r = len(self.arr)-1
+        while l < r:
+            if self.arr[l] >= 0:
+                l += 1
+            elif self.arr[r] < 0:
+                r -= 1
+            else:
+                self.arr[l], self.arr[r] = self.arr[r], self.arr[l]
+                l += 1
+                r -= 1
+
+        return self.arr
+
+
+#PartProblem = PartitionProblems([1, 1, 2, 2, 3, 3]) 
+PartProblem = PartitionProblems([1, 0, 0, 1, 2, 0]) 
+#PartProblem = PartitionProblems([1, -2, 3, -4, 5, 1, 5, -2]) 
+#first = PartProblem.rem_element(3)
+#second = PartProblem.rem_dublicates()
+#third = PartProblem.negative_partition()
+#forth = PartProblem.mvNeg_slow_fast()
+fifth = PartProblem.mvNeg_opposite_pointer()
+print(fifth, "yo how are you!")
+
+
+#opr = PartProblem.optimal_two()
+opr = PartProblem.optimal_one()
+#opr = PartProblem.rotate_zero()
+#opr = PartProblem.brute_force() # it has some issues, need to check and correct it!
+print(opr, "move zeros as optimal approach")
+
+
+
+
+class QueryIT:
+    def __init__(self, arr):
+        self.arr = arr
+
+    def second_largest(self):
+        largest = float("-inf")
+        ans = largest
+
+        for x in self.arr:
+            if largest > x > ans:
+                ans = x
+            elif x > largest:
+                ans = largest
+                largest = x
+
+        return ans
     
-    #mvNgLeft(arr)
-    if not isSap(arr): 
-        mvNgLeft(arr)
+    #kth means the first, second or third.. etc. we gonna try different approaches. then we'll move to optimal one.
+    def kth_bruteforce(self, k):
+        n = len(self.arr)
+        self.arr.sort()
+        return self.arr[n-k]
+        
+    def quick_select_kth(self, k):
+
+        def partit(self, arr, left, right):
+            idx = left-1; pivot = arr[right]
+            for x in arr:
+                if x <= pivot:
+                    idx += 1
+                    x, arr[idx] = arr[idx], x
+
+            idx += 1
+            arr[idx], pivot = pivot, arr[idx]
+
+            return idx
+
+        def quick_select(self, arr, left, right):
+            if left < right:
+                p_idx = partit(arr, left, right)
+                quick_select(arr, left, p_idx-1)
+                quick_select(arr, p_idx+1, right)
+
+            return 
+
+
+
+def partition(arr: list[int], left: int, right: int) -> int:
+    idx = left-1; pivot = arr[right] 
+    for i in range(left, right):
+        if arr[i] <= pivot:
+            idx += 1
+            arr[i], arr[idx] = arr[idx], arr[i]
+
+    idx += 1
+    print(idx)
+    arr[right], arr[idx] = arr[idx], arr[right]
+
+    return idx
+
+def quick_sort(arr, left, right):
+    if left <= right:
+        p_idx = partition(arr, left, right) #get the idx 
+        quick_sort(arr, left, p_idx-1)
+        quick_sort(arr, p_idx+1, right)
+
+    return arr
+
     
-    return arr
+arr = [3,1,2,4]
+print(quick_sort(arr, 0, len(arr)-1), "fucking fuck the fucker.....")
 
 
-arr = [1, -2, 3, -4, 5]
-def moveNeg(arr): #fast and slow pointer.
-    n = len(arr)
-    s = 0
-    for i in range(n):
-        if arr[i] >= 0:
-            arr[i], arr[s] = arr[s], arr[i]
-            s += 1
+query_it = QueryIT([1, 129, 2, 75, 92, 3, 1, 7])
+#opr = query_it.second_largest()
+opr = query_it.kth_bruteforce(2)
+print(opr, " -----------------------------  the kth largest value!")
 
-    return arr
-print(moveNeg(arr), "fuck it")
 
-arr = [1, -2, 3, -4, 5]
-def mvNeg(arr): #two opposite pointers.
-    left = 0
-    right = len(arr)-1
-    while left < right:
-        if arr[left] >= 0:
-            left += 1
-        elif arr[right] < 0:
-            right -= 1
-        else:
-            arr[left], arr[right] = arr[right], arr[left]
-            left += 1
-            right -= 1
 
-    return arr
 
-print(mvNeg(arr), "yooo")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -478,3 +582,15 @@ def remDubliBrute(arr): #bruteforce approach:
 
     return res
 print(remDubliBrute(arr), "something is wrong!")
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
